c0045d4af383768f74f87d8e3bbfdabf64125c1f
jnzm <98655840+jnzm@users.noreply.github.com>
2023-06-07 11:06:31 -0400

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Jahin Zashim on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
